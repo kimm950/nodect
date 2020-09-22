1f831541bab34d661a20c3c544faa591ac12a8a2
Kimm <hyunsung.kim@zeals.co.jp>
2020-09-22 22:32:30 +0900

    install nodemon

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send({ greeting: 'Hello React x Node' });
+  res.send({
+    greeting: 'Hello React x Node',
+    react: 'React!',
+    node: 'Node!'
+  });
 });
 
 module.exports = router;
