619d4823c9d388f935a049c1444578a921268dec
Kimm <hyunsung.kim@zeals.co.jp>
2020-09-22 22:30:24 +0900

    add cors then fetch data from express

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const callAPI = () => {
+    fetch("http://localhost:3002/api")
+      .then(res => res.text())
+      .then(res => setApiRes(res))
+  }
+
+  useEffect(() => {
+    callAPI();
+    console.log('sup');
+  }, [])
+  const [apiRes, setApiRes] = useState('')
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {apiRes}
         </p>
         <a
           className="App-link"
